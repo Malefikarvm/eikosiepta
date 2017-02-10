0885cc2f3064886f660bcc9e342858950044706e
Santiago Posada <jsposada@alegra.com>
2017-02-10 10:22:10 -0500

    method improved

--- main.cpp
+++ main.cpp
@@ -1,34 +1,21 @@
 #include <iostream>
-#include <stdlib.h>
 
 using namespace std;
 
 /**
  * Theory test
  * Project name : {eikos epta}
- * eq: |=> n^i=n^i-1(n) <-> i>0
- * -eq: |=> n^i=n^i-1/(n) <-> i<0
+ * eq  : |=> i>0 -> n^i=n^i-1*(n)
+ * -eq : |=> i<0 -> n^i=n^i+1/(n)
  */
 int64_t pow(int64_t n, int64_t i) {
     int64_t p = 1;
-    if (i != 0) {
-        if (i < 0){
-            p = pow(n, i+1);
-        } else if (i > 0) {
-            p = pow(n, i-1);
-        }
-    } else {
-        return p;
-    }
-    if (i < 0) {
-        return p / n;
-    } else if (i>0) {
-        return p * n;
-    }
+    p = (i < 0) ? (pow(n, i + 1)) / n : ((i > 0) ? (pow(n, i-1)) * n : p);
+    return p;
 }
 
 int main() {
-    cout << pow(2, 31) << endl;
+    cout << pow(3, 3) << endl;
     //system("PAUSE");
     return EXIT_SUCCESS;
 }
