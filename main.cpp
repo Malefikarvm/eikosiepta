4057fdb95fe36ac4e0e17546e129b6d916850500
Santiago Posada <jsposada@alegra.com>
2017-02-10 15:38:43 -0500

    improving the return :O (what a discovery)

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
 
 /**
  * Theory test
- * Project name : {eikos epta}
+ * Project name : {eikosi eptá}
  * eq  : |=> i>0 -> n^i=n^i-1*(n)
  * -eq : |=> i<0 -> n^i=n^i+1/(n)
  */
 int64_t pow(int64_t n, int64_t i) {
     int64_t p = 1;
-    p = (i < 0) ? (pow(n, i + 1)) / n : ((i > 0) ? (pow(n, i-1)) * n : p);
-    return p;
+    return (i < 0) ? (pow(n, i + 1)) / n : ((i > 0) ? (pow(n, i-1)) * n : p);
 }
 
 int main() {
     cout << pow(3, 3) << endl;
+    /*
+     * this was tested in linux using the clion output for the build, so fuck the system
+     */
     //system("PAUSE");
     return EXIT_SUCCESS;
 }
